9dc40f57927829b17effe00b19fd081eee9f6fe9
Conor Sullivan <conor@conorsullivan.net>
2024-12-17 14:58:29 -0600

    Basic UI elements for composition

--- src/SaveMoment.tsx
+++ src/SaveMoment.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import queryService from './database/queryService';
+import Slider from '@react-native-community/slider';
+import { useNavigation } from '@react-navigation/native';
+import { TextInput } from 'react-native-gesture-handler';
 
 function SaveMoment():React.JSX.Element{
     async function saveNote(text:string){
         await queryService.saveNewMoment(text);
       }
+        const navigation = useNavigation();
+    
+    const [vibeValue,setVibeValue] = React.useState(2);
     const styles = StyleSheet.create({
         container: {
             display: 'flex',
@@ -18,8 +24,21 @@ function SaveMoment():React.JSX.Element{
     return(
         <View style={styles.container}>
             <Text>Vibe</Text>
+            <Slider
+                style={{width: 200, height: 40}}
+                minimumValue={0}
+                maximumValue={4}
+                step={1}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="#000000"
+                value={vibeValue}
+                onSlidingComplete={(newVibe:number)=>{setVibeValue(newVibe)}}
+            />
             <Text>Tags</Text>
-            <Button>Save</Button>
+            <TextInput/>
+            {
+            /*@ts-ignore*/}
+            <Button onPress={()=>navigation.reset({index:0, routes:[{name:"Home"},{name:"View Moments"},{name:"Settings"},{name:"Export Data"}]})}>Save</Button>
         </View>
     )
 }
